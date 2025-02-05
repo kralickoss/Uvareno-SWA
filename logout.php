f7e5902e746b76c3ddc0b12472d9b96ac1485032
kralickoss <144122015+kralickoss@users.noreply.github.com>
2025-02-05 11:10:54 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start(); // Spustí nebo pokračuje existující relaci
-session_unset(); // Smaže všechny proměnné v relaci
-session_destroy(); // Zničí relaci
-
-// Přesměrování na přihlašovací stránku po odhlášení
-header("Location: index.php");
-exit;
-?>
