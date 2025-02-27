bc40345dae667c605e964878b4280096e4f9d9cb
kralickoss <144122015+kralickoss@users.noreply.github.com>
2025-02-27 13:17:39 +0100

    Delete Projekt1 directory

--- Projekt1/admin_navbar.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="styles.css">
-
-<nav>
-    <a href="index.php">Úvod</a>
-    <a href="add_recipe.php">Přidat recept</a>
-    <a href="recipe.php">Seznam receptů</a>
-    <a href="admin_dashboard.php">Admin Dashboard</a>
-    
-</nav>
